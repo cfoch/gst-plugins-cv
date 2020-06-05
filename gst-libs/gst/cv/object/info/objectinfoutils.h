7bb746a636dee3f3d0f6a4c0ba33c4af9f1777a7
Fabian Orccon <cfoch.fabian@gmail.com>
2020-06-05 13:30:30 -0500

    cvobjectdetect: Put coordinates of detected faces in a buffer meta
    In order to get other elements to know the coordinates and index
    for each detected object, a new GstCVObjectInfo is created with the
    following base sub key:
    
     - element(string)=<element_name> (*)
     - index(int)=%d
     - type(int)="roi"
    
    This sub key is extended with the "sub-key" parameter to create the
    full key. A subkey can be for example:
    
     - label="face"
     - face-angle="frontal"
     (or whatever)
    
    (*) The element field is added so downstream elements like
    gstcvdraw can in the future draw solely on the output of a target
    element, or a new tracker element could track just based on the
    output of that element
    
    Closes #12

---
+++ gst-libs/gst/cv/object/info/objectinfoutils.h
@@ -0,0 +1,44 @@
+/*
+ * GStreamer Plugins CV
+ * Copyright (C) 2019 Cesar Fabian Orccon Chipana <cfoch.fabian@gmail.com>
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef __CV_OBJECT_INFO_UTILS_H__
+#define __CV_OBJECT_INFO_UTILS_H__
+
+G_BEGIN_DECLS
+
+#define GST_TYPE_CV_OBJECT_INFO_SUB_KEY_TYPE    G_TYPE_STRING
+#define GST_TYPE_CV_OBJECT_INFO_SUB_KEY_LABEL   G_TYPE_STRING
+#define GST_TYPE_CV_OBJECT_INFO_SUB_KEY_INDEX   G_TYPE_INT
+#define GST_TYPE_CV_OBJECT_INFO_SUB_KEY_ELEMENT G_TYPE_STRING
+
+/* keys */
+#define GST_CV_OBJECT_INFO_SUB_KEY_TYPE         "type"
+#define GST_CV_OBJECT_INFO_SUB_KEY_LABEL        "label"
+#define GST_CV_OBJECT_INFO_SUB_KEY_INDEX        "index"
+#define GST_CV_OBJECT_INFO_SUB_KEY_ELEMENT      "element"
+
+
+/* values */
+#define GST_CV_OBJECT_INFO_SUB_KEY_TYPE_ROI     "roi"
+
+
+G_END_DECLS
+
+#endif /* __CV_OBJECT_INFO_UTILS_H__ */
